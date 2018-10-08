6421456b5e4afdb68c45e2f167decccf230f70a0
kfarbaniec <farbaniec.karol@gmail.com>
2018-10-08 13:29:32 +0200

    Delta Calc developement finished!

---
+++ src/main/java/com/dynamics/andrzej/delta/gui/Delta.java
@@ -0,0 +1,13 @@
+package com.dynamics.andrzej.delta.gui;
+
+/**
+ * Created by farbanie_1115092 on 08.10.18.
+ */
+public class Delta {
+
+    public static double calculateDelta(double b, double a, double c ){
+
+        return b*b - 4*a*c;
+    }
+
+}
